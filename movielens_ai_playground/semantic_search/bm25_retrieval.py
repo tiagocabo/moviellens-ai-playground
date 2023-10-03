c80671d72125e114a469707dfbc411fb53a651fb
Tiago Cabo <tiago.cabo@olx.com>
2023-10-03 19:44:11 +0100

    clean bm25 retrieval

--- movielens_ai_playground/semantic_search/bm25_retrieval.py
+++ movielens_ai_playground/semantic_search/bm25_retrieval.py
@@ -4,8 +4,6 @@ import numpy as np
 import pandas as pd
 from rank_bm25 import BM25Okapi
 
-from movielens_ai_playground.semantic_search.bm25_preprocessing import nltk_process
-
 MOVIES_PATH = "/Users/tiago.cabo/Documents/github-repos/moviellens-ai-playground/data/ml-25m/movies.csv"
 movies_df = pd.read_csv(MOVIES_PATH)
 
@@ -19,19 +17,14 @@ pickle.dump(bm25_title, file=file)
 file = open("bm25_title", "rb")
 
 bm25_title = pickle.load(file)
-# print("done")
 
-def bm25_retrieve(query, hits, bm25_type = "bm25_title"):
-    # preprocess query
-    tokenized_query = nltk_process(query)
-    print(tokenized_query)
+
+def bm25_retrieve(query, hits, bm25_type="bm25_title"):
     # fetch bm25 score
     if bm25_type == "bm25_title_desc":
-        #doc_scores = bm25_title_desc.get_scores(tokenized_query)
         raise NotImplementedError
     elif bm25_type == "bm25_title":
-        doc_scores = bm25_title.get_scores(tokenized_query)
-        print(max(doc_scores))
+        doc_scores = bm25_title.get_scores(query)
     else:
         raise NotImplementedError("Please check bm25 type")
 
@@ -45,5 +38,6 @@ def bm25_retrieve(query, hits, bm25_type = "bm25_title"):
     bm25_top = bm25_df.movieId[: int(hits)].values
     return bm25_top
 
+
 if  __name__ == "__main__":
-    bm25_retrieve(query="car", hits=5, bm25_type="bm25_title")
+    bm25_retrieve(query="the", hits=5, bm25_type="bm25_title")
